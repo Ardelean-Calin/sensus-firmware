0944d4a9ae16989d6f90459091231a8d9f428985
Ardelean Calin <ardelean.calin@proton.me>
2022-09-23 15:22:56 +0300

    Disabled TWIM for now as I test power profiling.

--- src/main.rs
+++ src/main.rs
@@ -143,29 +143,29 @@ async fn main(spawner: Spawner) {
     let mut saadc = saadc::Saadc::new(p.SAADC, interrupt::take!(SAADC), config, [channel_cfg]);
     saadc.calibrate().await;
 
-    // Enable power to the sensors
-    sen.set_high();
-    // 0 -> 3.3V in less than 500us. Measured on oscilloscope. So I will set 2 ms just to be sure.
-    Timer::after(Duration::from_millis(2)).await;
+    // // Enable power to the sensors
+    // sen.set_high();
+    // // 0 -> 3.3V in less than 500us. Measured on oscilloscope. So I will set 2 ms just to be sure.
+    // Timer::after(Duration::from_millis(2)).await;
 
-    let mut irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
-    let mut i2c_config = twim::Config::default();
-    i2c_config.frequency = twim::Frequency::K250; // Middle ground between speed and power consumption.
-    i2c_config.scl_pullup = true;
-    i2c_config.sda_pullup = true;
+    // let mut irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
+    // let mut i2c_config = twim::Config::default();
+    // i2c_config.frequency = twim::Frequency::K250; // Middle ground between speed and power consumption.
+    // i2c_config.scl_pullup = true;
+    // i2c_config.sda_pullup = true;
 
-    let mut twi = Twim::new(
-        &mut p.TWISPI0,
-        &mut irq,
-        &mut p.P0_08,
-        &mut p.P0_09,
-        i2c_config,
-    );
+    // let mut twi = Twim::new(
+    //     &mut p.TWISPI0,
+    //     &mut irq,
+    //     &mut p.P0_08,
+    //     &mut p.P0_09,
+    //     i2c_config,
+    // );
 
-    let delay = embassy_time::Delay;
-    let mut shtc3 = shtcx::shtc3(twi);
-    let dev_id = shtc3.device_identifier().unwrap();
-    info!("Read dev_id: {}", dev_id);
+    // let delay = embassy_time::Delay;
+    // let mut shtc3 = shtcx::shtc3(twi);
+    // let dev_id = shtc3.device_identifier().unwrap();
+    // info!("Read dev_id: {}", dev_id);
 
     // Reading the device identifier is complete in about 4ms
     // Disable power to the sensors.
