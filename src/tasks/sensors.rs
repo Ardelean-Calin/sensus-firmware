4f5c6be1573ef43a41c6403b6bcfee920e364238
Ardelean Calin <ardelean.calin@proton.me>
2022-09-29 12:22:08 +0300

    Added SHTC3 driver.

--- src/tasks/sensors.rs
+++ src/tasks/sensors.rs
@@ -3,99 +3,92 @@ mod common;
 
 #[path = "../drivers/battery_sensor.rs"]
 mod battery_sensor;
+#[path = "../drivers/ltr303_sensor.rs"]
+mod ltr303_sensor;
+use embassy_nrf::interrupt::SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0;
+use embedded_hal_async::i2c::I2c;
+use futures::future::join;
+use shtc3_async::{Measurement, Shtc3};
 
 use defmt::{info, *};
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_nrf::saadc::Input;
-use embassy_time::{Duration, Timer};
-// use futures::future::join3;
+use embassy_nrf::gpio::{Input, Level, Output, OutputDrive, Pin, Pull};
+use embassy_nrf::twim::{self, Twim};
+use embassy_nrf::{interrupt, Peripherals};
+use embassy_time::{Delay, Duration, Timer};
+
+use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
+use embassy_sync::blocking_mutex::raw::{NoopRawMutex, RawMutex};
+use embassy_sync::mutex::Mutex;
 
 use battery_sensor::BatterySensor;
 
-struct Pins<T> {
-    adc_in: T,
-    i2c_sda: T,
-    i2c_scl: T,
-    sen: T,
-}
-struct Sensors<'a> {
-    // pins: Pins,
-    batt_sensor: Option<BatterySensor<'a>>,
+async fn shtc3_sample<'a, M: RawMutex, BUS: I2c>(dev: I2cDevice<'a, M, BUS>) -> Measurement {
+    let mut sht = Shtc3::new(dev);
+    let mut delay = Delay;
+
+    unwrap!(sht.wakeup(&mut delay).await);
+    let result = unwrap!(sht.measure(&mut delay).await);
+    info!(
+        "Got the following measurement: RH: {}%\tT: {}C",
+        result.humidity.as_percent(),
+        result.temperature.as_degrees_celsius()
+    );
+    unwrap!(sht.sleep().await);
+    result
 }
 
-impl<'a> Sensors<'a> {
-    /// TODO: Maybe add a pins configuration to this constructor.
-    /// Something along the lines of:
-    ///
-    /// Pins {
-    ///     adc_in: P0_29,
-    ///     i2c_sda: P0_08,
-    ///     i2c_scl: P0_09,
-    ///     sen: P0_06,
-    /// }
-    fn new() -> Self {
-        Sensors { batt_sensor: None }
+async fn sensors_sample(
+    p: &mut Peripherals,
+    irq1: &mut SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0,
+    irq2: &mut embassy_nrf::interrupt::SAADC,
+) {
+    let mut sen = Output::new(&mut p.P0_06, Level::Low, OutputDrive::Standard);
+    sen.set_high();
+    loop {
+        {
+            // Initialize and enable the power to the sensors. TODO! Remove from final version.
+            // let mut sen = Output::new(&mut p.P0_06, Level::Low, OutputDrive::Standard);
+            // sen.set_high();
+            // core::mem::drop(sen);
+            Timer::after(Duration::from_millis(2)).await;
+
+            // Configure used interfaces
+            // 1. ADC
+            let mut battery_sensor = BatterySensor::new(&mut p.P0_29, &mut p.SAADC, irq2);
+            battery_sensor.init().await;
+
+            // 2. TWIM
+            let mut config = twim::Config::default();
+            config.frequency = twim::Frequency::K250;
+
+            let i2c = Twim::new(
+                &mut p.TWISPI0,
+                &mut *irq1,
+                &mut p.P0_08,
+                &mut p.P0_09,
+                config,
+            );
+            let i2c_bus = Mutex::<NoopRawMutex, _>::new(i2c);
+            let i2c_dev1 = I2cDevice::new(&i2c_bus);
+            let i2c_dev2 = I2cDevice::new(&i2c_bus);
+
+            // let (shtc3_measurement, ltr303_measurement) = join(shtc3_sample(i2c_dev1), ltr303_sample(i2c_dev2));
+
+            let (shtc3_meas, batt_meas) =
+                join(shtc3_sample(i2c_dev1), battery_sensor.sample()).await;
+        }
+        Timer::after(Duration::from_millis(1000)).await;
     }
-
-    // Init all sensors
-    pub async fn init(&mut self) {
-        let p = embassy_nrf::init(Default::default());
-
-        let adc_pin = p.P0_29.degrade_saadc();
-        let batt = BatterySensor::new(adc_pin, p.SAADC);
-        batt.calibrate().await;
-
-        self.batt_sensor = Some(batt);
-    }
-
-    pub fn power_enable(&self) {}
-    pub fn power_disable(&self) {}
 }
-
-// struct SensorData {
-//     timestamp_s: u32, // Timestamp in seconds. Up to 136 years.
-//     air_temp: u16,    // 100 millikelvin per unit <=> 0K to 6553K
-//     air_relhum: u16,  // 100 millipercent per unit <=> 0.0% to 100.0%
-//     irradiance: u16,  // Solar irradiance in Lux
-//     soil_temp: u16,   // 100 millikelvin per unit <=> 0K to 6553K
-//     soil_freq: u32,   // frequency for now, percentages in the future.
-// }
-
-// impl Default for SensorData {
-//     fn default() -> Self {
-//         Self {
-//             timestamp_s: 0,
-//             air_temp: 0,
-//             air_relhum: 0,
-//             irradiance: 0,
-//             soil_temp: 0,
-//             soil_freq: 0,
-//         }
-//     }
-// }
 
 #[embassy_executor::task]
 pub async fn sensors_task() {
-    let mut sensors = Sensors::new();
-    sensors.init().await;
-    let mut batt_sensor = unwrap!(sensors.batt_sensor);
-    // let mut sen = Output::new(p.P0_06, Level::Low, OutputDrive::Standard);
-    loop {
-        // Enable power to the sensors.
-        // sen.set_high();
-        // 0 -> 3.3V in less than 500us. Measured on oscilloscope. So I will set 2 ms just to be sure.
-        Timer::after(Duration::from_millis(2)).await;
+    let mut config = embassy_nrf::config::Config::default();
+    config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
+    // Peripherals config
+    let mut p = embassy_nrf::init(config);
+    let mut i2c_irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
+    let mut adc_irq = interrupt::take!(SAADC);
 
-        // let (battery_data, shtc3_data) = join!(battery_sample(), shtc3_sample()).await;
-        // Wait for all data aquisition to finish and then deconstruct the data
-        // let (shtc3_data, ltr303_data, soil_probe_data) =
-        //     futures::future::join!(shtc3_sample, ltr303_sample, soil_probe_sample).await;
-
-        batt_sensor.sample().await;
-        // info!("sensors_task tick!");
-        Timer::after(Duration::from_millis(500)).await;
-
-        // Disable power to the sensors.
-        // sen.set_low();
-    }
+    sensors_sample(&mut p, &mut i2c_irq, &mut adc_irq).await;
 }
