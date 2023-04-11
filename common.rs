85aee772ba5062a2a593e15310b9974d5659bb98
Ardelean Calin <artech@duck.com>
2023-04-11 15:21:16 +0300

    Added panic-reset in release mode.

--- common.rs
+++
@@ -1,9 +0,0 @@
-#![macro_use]
-
-use defmt_rtt as _; // global logger
-use embassy_nrf as _; // time driver
-
-#[cfg(debug_assertions)]
-use panic_probe as _;
-#[cfg(not(debug_assertions))]
-use panic_reset as _; // panic handler
--- src/prelude.rs
+++ src/prelude.rs
@@ -3,7 +3,7 @@
 use defmt_rtt as _; // global logger
 use embassy_nrf as _; // time driver
 
-#[cfg(not(debug_assertions))]
-use panic_persist as _; // panic handler that logs error into RAM, then soft-resets
 #[cfg(debug_assertions)]
 use panic_probe as _;
+#[cfg(not(debug_assertions))]
+use panic_reset as _; // panic handler that logs error into RAM, then soft-resets
